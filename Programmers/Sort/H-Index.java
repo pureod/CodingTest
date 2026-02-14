5bc256c2b24684f7701367e1aa19c594ac1e9a68
pureod <sjo06102@gmail.com>
2026-02-14 16:04:11 +0900

    42747 H-Index lv2 68% (fail)

---
+++ Programmers/Sort/H-Index.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int n = citations.length;
+        Arrays.sort(citations);
+
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= (n - i)) return n - i;
+        }
+
+        return 0;
+    }
+}
