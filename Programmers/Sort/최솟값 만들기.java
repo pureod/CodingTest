f81bec66e2c82566f2e0808b00674b90f9697fd2
pureod <sjo06102@gmail.com>
2026-02-25 22:57:11 +0900

    12941 최솟값 만들기 lv2 79%

---
+++ Programmers/Sort/최솟값 만들기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int []A, int []B) {
+        int n = A.length;
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            answer += A[i] * B[n-i-1];
+        }
+
+        return answer;
+    }
+}
